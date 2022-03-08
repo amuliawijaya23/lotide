d261a54d74e1f38483b75fc51f5047c118321313
amuliawijaya23 <amuliawijaya@gmail.com>
2022-03-07 16:47:59 -0800

    created an equal checker function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('Assertion Passed: ' + actual + ' === ' + expected + ' ✅');
+  } else {
+    console.log('Assertion Failed: ' + actual + ' !== ' + expected + ' 🛑');
+  }
+};
+
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(0, '0');
+assertEqual(0, false);
+assertEqual(true, 'true');
+assertEqual(false, false);
