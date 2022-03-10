a35c8207563e6be82884e4801962e1369b3d3062
amuliawijaya23 <amuliawijaya@gmail.com>
2022-03-09 21:48:43 -0800

    assertion equality for 2 objects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected} ✅`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected} 🛑`);
-  }
-};
-
-
 const eqArrays = function(array1, array2) {
   let isEqual = false;
   if (array1.length === array2.length) {
@@ -39,30 +30,23 @@ const eqObjects = function(firstObject, secondObject) {
   }
   return true;
 };
-    
-    
 
-
+const assertObjectsEqual = function (objectOne, objectTwo) {
+  const inspect = require('util').inspect;
+  if ((eqObjects(objectOne, objectTwo)) === true) {
+    console.log(`Assertion Passed: ${inspect(objectOne)} === ${inspect(objectTwo)} ✅`);
+  } else {
+    console.log(`Assertion Failed: ${inspect(objectOne)} !== ${inspect(objectTwo)} 🛑`);
+  }
+};
 
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 const abc = { a: "1", b: "2", c: "3" };
 
-const cb = {a: "1", b: "4"}
-const jb = {a: "2", b: "4"}
-const jd = {a: "1", b: "2"}
-
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-
-
-assertEqual(eqObjects(ab, ba), true);
-assertEqual(eqObjects(ab, abc), false);
-assertEqual(eqObjects(cb, jb), false);
-assertEqual(eqObjects(jb, jd), false);
-
-assertEqual(eqObjects(cd, dc), true);
-assertEqual(eqObjects(cd, cd2), false);
+assertObjectsEqual(ab, ba);
+assertObjectsEqual(ab, abc);
