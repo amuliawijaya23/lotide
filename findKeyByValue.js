5ee0afd9ac0c0857bff8fdb1aafee72b8ef5faa0
amuliawijaya23 <amuliawijaya@gmail.com>
2022-03-09 19:05:41 -0800

    finding object the object properties of certain value

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected} ✅`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected} 🛑`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    } 
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
